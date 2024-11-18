d6c1f3e693452cba5952063a7361fc5898f31024
PulseBeat_02 <brandonli2006ma@gmail.com>
2024-11-17 17:30:35 -0800

    Changes

--- pages/_document.js
+++ pages/_document.js
@@ -4,22 +4,23 @@ export default function Document() {
   const meta = {
     title: "Brandon Li",
     description: "Brandon Li's Developer Portfolio",
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    image: './images/icon.webp'
   }
   return (
     <Html lang="en">
-      <Head>
-        <meta name="robots" content="follow, index" />
-        <meta name="description" content={meta.description} />
-        <meta property="og:site_name" content={meta.title} />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+        <Head>
+            <meta name="robots" content="follow, index"/>
+            <meta name="description" content={meta.description}/>
+            <meta property="og:site_name" content={meta.title}/>
+            <meta property="og:description" content={meta.description}/>
+            <meta property="og:title" content={meta.title}/>
+            <meta property="og:image" content={meta.image}/>
+            <link rel="icon" href={meta.image}/>
+        </Head>
+        <body>
+        <Main/>
+        <NextScript/>
+        </body>
     </Html>
   )
 }
